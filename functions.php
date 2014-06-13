26639dd32e2ef5fd13b5c3085ffc28ee86de5920
Devin Price <devin@wptheming.com>
2014-06-13 16:11:57 -0500

    Update typography.

--- functions.php
+++ functions.php
@@ -87,8 +87,8 @@ function platform_widgets_init() {
 		'description'   => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 }
 add_action( 'widgets_init', 'platform_widgets_init' );
--- sidebar.php
+++ sidebar.php
@@ -13,14 +13,14 @@
 			</aside>
 
 			<aside id="archives" class="widget">
-				<h1 class="widget-title"><?php _e( 'Archives', 'platform' ); ?></h1>
+				<h3 class="widget-title"><?php _e( 'Archives', 'platform' ); ?></h3>
 				<ul>
 					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 				</ul>
 			</aside>
 
 			<aside id="meta" class="widget">
-				<h1 class="widget-title"><?php _e( 'Meta', 'platform' ); ?></h1>
+				<h3 class="widget-title"><?php _e( 'Meta', 'platform' ); ?></h3>
 				<ul>
 					<?php wp_register(); ?>
 					<li><?php wp_loginout(); ?></li>
